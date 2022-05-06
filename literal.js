b4ab3fcacf116990486f48363dd757e42de0b2ec
Naggayi Daphne <daphnepearl101@gmail.com>
2022-05-05 23:59:37 -0700

    number literals

---
+++ literal.js
@@ -0,0 +1,11 @@
+// types pf literals
+
+
+// 1. Variable declarations 
+var myAge;
+
+//2.  variable definition [assignment]
+myAge = 120; // creating a number literal
+
+// 3. using a variABLE
+console.log(myAge);
---
+++ literal.ts
@@ -0,0 +1,10 @@
+// 1. Variable declarations 
+var myAge:Number; 
+
+//2.  variable definition [assignment]
+myAge = 120; 
+
+// 3. using a variABLE
+console.log(myAge)
+
+
