2ab32a2707ad67902e0928d9d29ceff2f50ba0d1
Naggayi Daphne <daphnepearl101@gmail.com>
2022-05-06 04:28:40 -0700

    abstraction

--- oop.ts
+++ oop.ts
@@ -36,7 +36,7 @@ class Payment {
 // cash inherits all public variables in payment
 // protected makes the propert private and only accessible in the class Protected
 class Cash extends Payment{
-    protected cashTendered: any
+    private cashTendered: any
 }
 // multilevel inheritance is involvement of one or more classes
 class Check extends Payment {
@@ -55,5 +55,37 @@ class Credit extends Payment {
     authorized() {return null}
 }
 
-let pm1 = new Payment(); 
-pm1.amount()
+// let pm1 = new Payment(); 
+// pm1.amount()
+
+
+// abstraction hides implementation actions from users
+class DigestiveSystem {
+    // data
+    private food;
+
+    // methods
+    public putFoodinMouth(food:string){
+        this.ingestion(food)
+    }
+    
+    private ingestion(food:string) {
+        this.pelisty(food)
+    }
+    private pelisty(bolus:string){
+        this.absorption(bolus)
+    }
+    private absorption(xxx:string){
+        this.ejection(yyy)
+    }
+    private ejection(yyy:string){
+        this.easeYourSelf()
+    }
+
+    public easeYourSelf(zzz: string){
+        console.log('long call.. ');
+        console.log(zzz);
+        
+        
+    }
+}
