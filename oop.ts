9cb5736b25a3134f0099a21435e2cc544617d519
Naggayi Daphne <daphnepearl101@gmail.com>
2022-05-06 02:01:15 -0700

    creating a class

---
+++ oop.ts
@@ -0,0 +1,12 @@
+class Customer {
+    // data/ attribute
+    private name:string
+    private address:any
+    // operators/ methods/behaviours that operate on data
+    constructor(name:string, address:string){
+        this.name = name; 
+        this.address = address;
+    }
+}
+
+// Instaniate a class
