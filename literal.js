09341d53e8deeab6e7f0bc7f14fd62380fe1e910
Naggayi Daphne <daphnepearl101@gmail.com>
2022-05-06 00:12:13 -0700

    object literal

--- literal.js
+++ literal.js
@@ -1,6 +1,9 @@
 // 1. Variable declarations 
 var bootcamp;
 //2.  variable definition [assignment]
-bootcamp = ["daphne", "kylie"]; // array literal 
+bootcamp = {
+    name: 'Pearl',
+    age: 24
+}; // object literal 
 // 3. using a variABLE
 console.log(bootcamp);
--- literal.ts
+++ literal.ts
@@ -1,8 +1,11 @@
 // 1. Variable declarations 
-var bootcamp:Array <string>; 
+var bootcamp:object; 
 
 //2.  variable definition [assignment]
-bootcamp = ["daphne", "kylie"]; // array literal 
+bootcamp = {
+    name: 'Pearl',
+    age: 24
+} // array literal 
 
 // 3. using a variABLE
 console.log(bootcamp)
