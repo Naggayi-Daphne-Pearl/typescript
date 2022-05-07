4f149bf9bc948a97c7ab242f386ac12401fa59f3
Naggayi Daphne <daphnepearl101@gmail.com>
2022-05-07 09:04:59 -0700

    POLYMORPHISM -OVER-RIDING

--- oop.ts
+++ oop.ts
@@ -21,11 +21,9 @@ class Customer {
 //     }
 // }
 // console.log(concatenate(firstName, lastName));
-
-
 class Payment {
     private amount: any
-    pay() {
+     pay() {
         console.log('generic payment');
         
     }
@@ -37,6 +35,7 @@ class Payment {
 // protected makes the propert private and only accessible in the class Protected
 class Cash extends Payment{
     private cashTendered: any
+    
 }
 // multilevel inheritance is involvement of one or more classes
 class Check extends Payment {
@@ -60,32 +59,93 @@ class Credit extends Payment {
 
 
 // abstraction hides implementation actions from users
-class DigestiveSystem {
-    // data
-    private food;
+// class DigestiveSystem {
+//     // data
+//     private food;
 
-    // methods
-    public putFoodinMouth(food:string){
-        this.ingestion(food)
-    }
+//     // methods
+//     public putFoodinMouth(food:string){
+//         this.ingestion(food)
+//     }
     
-    private ingestion(food:string) {
-        this.pelisty(food)
-    }
-    private pelisty(bolus:string){
-        this.absorption(bolus)
-    }
-    private absorption(xxx:string){
-        this.ejection(yyy)
-    }
-    private ejection(yyy:string){
-        this.easeYourSelf()
-    }
+//     private ingestion(food:string) {
+//         this.pelisty(food)
+//     }
+//     private pelisty(bolus:string){
+//         this.absorption(bolus)
+//     }
+//     private absorption(xxx:string){
+//         this.ejection(yyy)
+//     }
+//     private ejection(yyy:string){
+//         this.easeYourSelf()
+//     }
 
-    public easeYourSelf(zzz: string){
-        console.log('long call.. ');
-        console.log(zzz);
+//     public easeYourSelf(zzz: string){
+//         console.log('long call.. ');
+//         console.log(zzz);
         
+        
+//     }
+// }
+
+// only objects instanitated with the components of check class
+// this is a form of abstraction c
+let myPaymentMethod: Check = new Check()
+
+
+// Polymorphism 
+class Character {
+    hunger: number
+    health:number
+    attack (){
+        console.log('i am attacking');
+        
+    }
+    move() {
+        console.log('I am moving');
+        
+    }
+    eat () {
+        console.log('I am eating');
+        
+    }
+} 
+class Archer extends Character {
+    arrows: number
+    // overriding the superclass behaviour
+    attack() {
+        super.attack()
+        console.log('I am firing an arrow')
+        this.arrows
         
     }
 }
+
+class Mage extends Character {
+    mana: number 
+    attack() {
+        super.attack()
+        console.log('Throwimg a potion')
+        this.mana -= 1
+        
+
+    }
+}
+
+class Knight extends Character{
+    shield: number
+    attack() {
+        super.attack()
+        console.log('i am a kinight');
+        
+    }
+}
+
+const archer = new Archer()
+const mage = new Mage()
+const knight = new Knight()
+
+archer.attack()
+mage.attack()
+knight.attack()
