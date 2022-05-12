69e7787525b7dec95acb0405f11b5c792a1c3bc1
Naggayi Daphne <daphnepearl101@gmail.com>
2022-05-12 03:04:54 -0700

    dependency inversion policy

--- solid.ts
+++ solid.ts
@@ -5,23 +5,35 @@
 // computer
 // single responsiblity principle (spr)a class should have one reason to change 
 // SPR FOR inputing keyboard. Whatever is in the class should have a purpose to fulfill that purpose. 
-class Keyboard {
-    inputDatafromKeyboard(data:any) {
-        console.log(data);
+// Dependenct Inversion Policy 
+
+
+interface InputDevice {
+    inputData():any
+}
+class Keyboard implements InputDevice {
+    inputData() {
+       return "data"
         
     }
 }
-class InMemory{
+interface Memory {
+    storeData(data:any):boolean
+    retrieveData(basis:any):any
+}
+class InMemory implements Memory{
     memory: Array<any> = []
     
-    store(data:any){
+    storeData(data:any){
         this.memory.push(data)
+        return true
     }
-    retrieve (data:any) {
+    retrieveData (data:any) {
         this.memory.push(data)
     }
 
 }
+
 class CPU {
     process(data:string){
         let information = 'Processed: ' + data 
@@ -29,7 +41,9 @@ class CPU {
     }
     
 }
-
+interface DisplayDevice {
+    display (data:any)
+}
 class Monitor {
     display(data:string){
         console.log('Displaying: ' + data + 'on monitor');
@@ -45,37 +59,44 @@ class ErrorHandler {
 class Computer {
     //data 
     memory: Array<any> = []; 
+    // this is high level dependecy
+    inputDevice:InputDevice; 
+    memory:Memory;
+    processor:CPU;
+    displayDevice:DisplayDevice; 
+    errorHandler:ErrorHandler= new ErrorHandler();  
+    
+
     // methods 
+    constructor( inputDevice:InputDevice, memory:Memory,   processor:CPU, displayDevice:DisplayDevice ){
+       this.inputDevice = inputDevice
+       this.memory =  memory
+       this.processor = processor
+       this.displayDevice = displayDevice 
+    }
     input() {
-        let inputMethod = new Keyboard(); 
-        inputMethod.inputDatafromKeyboard('data')
+        return this.inputDevice.inputData()
         
     }
     store(data:any){
-        // Delegate 
-        let inMemory = new InMemory()
-        inMemory.store(data)       
-        
+        this.memory.storeData(data)           
     }
     retrieve(data:any) {
-        let inMemory = new InMemory();
-        return inMemory.retrieve
+        return this.memory.retrieveData(null)
        
         
     }
-    process(data:any) {
-        let processor = new CPU(); 
-        processor.process(data)
+    process(data:any) { 
+        this.processor.process(data)
         
     }
     display(data:any) {
-       let monitor = new Monitor(); 
-       monitor.display(data) 
+      
+       this.displayDevice.display(data) 
         
     }
-    handleError(error:any) {
-        let errorHandler = new ErrorHandler();  
-        errorHandler.handleError(error)
+    handleError(error:any) { 
+        this.errorHandler.handleError(error)
         
     }
 }
@@ -86,4 +107,5 @@ class Computer {
 
 // test
 let computer = new Computer(); 
+computer.displayDevice = new Monitor()
 computer.process('fffff')
