daeed50253205e5a2977f30c927a12b20d582541
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2022-05-13 09:35:50 +0300

    dependency inversion principle

--- solid.ts
+++ solid.ts
@@ -1,111 +1,135 @@
-// computer qualities 
+// Calculator
+    //  add, sub, mult, normal division, modulus division, outpution   
 
+// Computer
+    // pluging into power source
+    // Powring on / off
+    // Loading Programs
+    // inputs data through the keyboard
+    // stores data to in-memory
+    // processes data in the CPU
+    // outputs data on a monitor
+    // Handles errors and exceptions
 
+/*
+    Computer
+        - inputs data through the keyboard
+        - stores data to in-memory
+        - retrieve data from in-memory
+        - processes data in the CPU
+        - displaying information on a monitor
+        - Handles errors and exceptions
 
-// computer
-// single responsiblity principle (spr)a class should have one reason to change 
-// SPR FOR inputing keyboard. Whatever is in the class should have a purpose to fulfill that purpose. 
-// Dependenct Inversion Policy 
+*/ 
 
+// High Cohesion: How focused the class is?
+// Loosely Cooped Designs?
+// Dependency Inversion Principle:
+    // High Level Modules should not depend on low level modules, but instead both should depend on Abstractions
 
-interface InputDevice {
-    inputData():any
-}
-class Keyboard implements InputDevice {
-    inputData() {
-       return "data"
-        
-    }
-}
-interface Memory {
-    storeData(data:any):boolean
-    retrieveData(basis:any):any
-}
-class InMemory implements Memory{
-    memory: Array<any> = []
-    
-    storeData(data:any){
-        this.memory.push(data)
-        return true
-    }
-    retrieveData (data:any) {
-        this.memory.push(data)
-    }
-
-}
-
-class CPU {
-    process(data:string){
-        let information = 'Processed: ' + data 
-        return information
+    interface InputDevice{
+        inputData():any;
     }
     
-}
-interface DisplayDevice {
-    display (data:any)
-}
-class Monitor {
-    display(data:string){
-        console.log('Displaying: ' + data + 'on monitor');
+    class KeyBoard implements InputDevice{
+        // Methods
+        inputData(){
+            return "data"
+        }
+    }
+    interface Memory {
+        storeData(data:any): boolean;
+        retrieveData(basis:any): any;
+    }
+    
+    class InMemory implements Memory{
+        memory:Array<any> = [];
+    
+        storeData(data:any){
+            this.memory.push(data);
+            return true;
+        }
+    
+        retrieveData(){
+            return this.memory
+        }
+    }
+    
+    
+    class CPU {
+        process(data:string){
+            let information = "Processed: " + data
+            return information
+        }
+    }
+    interface DisplayDevice{
+        display(data:any): boolean;
+    }
+    
+    class Monitor implements DisplayDevice{
+        // Methods
+        display(data:any){
+            console.log("Displaying "+ data + "on monitor.");
+            return true;
+        }
+    }
+    
+    
+    class ErrorHandler{
+        handleError(error:any){
+            console.log("Error: " + error);
+        }
     }
 
-}
-class ErrorHandler {
-    handleError(error:any){
-        console.log('error: ' + error);
-        
-    }
-}
+
 class Computer {
-    //data 
-    memory: Array<any> = []; 
-    // this is high level dependecy
-    inputDevice:InputDevice; 
+    // Data
+    inputDevice:InputDevice;
     memory:Memory;
     processor:CPU;
-    displayDevice:DisplayDevice; 
-    errorHandler:ErrorHandler= new ErrorHandler();  
-    
+    diplayDevice:DisplayDevice;
+    errorHandler:ErrorHandler  = new ErrorHandler();
 
-    // methods 
-    constructor( inputDevice:InputDevice, memory:Memory,   processor:CPU, displayDevice:DisplayDevice ){
-       this.inputDevice = inputDevice
-       this.memory =  memory
-       this.processor = processor
-       this.displayDevice = displayDevice 
+
+    // Methods
+    constructor(inputDevice:InputDevice, memory:Memory, processor:CPU, diplayDevice:DisplayDevice){
+        this.inputDevice = inputDevice;
+        this.memory = memory;
+        this.processor = processor;
+        this.diplayDevice = diplayDevice;
     }
-    input() {
-        return this.inputDevice.inputData()
-        
+
+    input(){
+       return this.inputDevice.inputData();
     }
-    store(data:any){
-        this.memory.storeData(data)           
+
+    store(data:any):boolean{
+        // Delegate
+        return this.memory.storeData(data);
     }
-    retrieve(data:any) {
+
+    retrieve(data:any){
         return this.memory.retrieveData(null)
-       
-        
     }
-    process(data:any) { 
-        this.processor.process(data)
-        
+
+    process(data:any){
+       this.processor.process(data)
     }
-    display(data:any) {
-      
-       this.displayDevice.display(data) 
-        
+
+    display(data:any){
+        return this.diplayDevice.display(data)
     }
-    handleError(error:any) { 
+
+    handleError(error:any){
         this.errorHandler.handleError(error)
-        
+
     }
+
 }
 
 
-
-
-
-// test
-let computer = new Computer(); 
-computer.displayDevice = new Monitor()
-computer.process('fffff')
+// Test
+let inputDevice = new KeyBoard()
+// let computer = new Computer()
+// computer.diplayDevice = new Monitor();
+// computer.display("fffff");
